a29b104a576f88e2f1a324cbd48866ca05b07ae1
Bronson Zgeb <bronson.zgeb@unity3d.com>
2021-06-28 21:11:16 -0400

    Added brush and background colour

--- Assets/GPUDrawingApp/DrawManager.cs
+++ Assets/GPUDrawingApp/DrawManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class DrawManager : MonoBehaviour
 {
     [SerializeField] ComputeShader _drawComputeShader;
+    [SerializeField] Color _backgroundColour;
+    [SerializeField] Color _brushColour;
     RenderTexture _canvasRenderTexture;
 
     void Start()
@@ -11,7 +13,12 @@ public class DrawManager : MonoBehaviour
         _canvasRenderTexture.filterMode = FilterMode.Point;
         _canvasRenderTexture.enableRandomWrite = true;
         _canvasRenderTexture.Create();
-        
+
+        int initBackgroundKernel = _drawComputeShader.FindKernel("InitBackground");
+        _drawComputeShader.SetVector("_BackgroundColour", _backgroundColour);
+        _drawComputeShader.SetTexture(initBackgroundKernel, "_Result", _canvasRenderTexture);
+        _drawComputeShader.Dispatch(initBackgroundKernel, _canvasRenderTexture.width / 8,
+            _canvasRenderTexture.height / 8, 1);
         /*
         int mainKernel = _drawComputeShader.FindKernel("CSMain");
         _drawComputeShader.SetTexture(mainKernel, "_Result", _canvasRenderTexture);
@@ -24,7 +31,9 @@ public class DrawManager : MonoBehaviour
     {
         int updateKernel = _drawComputeShader.FindKernel("Update");
         _drawComputeShader.SetVector("_MousePosition", Input.mousePosition);
+        _drawComputeShader.SetBool("_MouseDown", Input.GetMouseButton(0));
         _drawComputeShader.SetFloat("_BrushSize", 10f);
+        _drawComputeShader.SetVector("_BrushColour", _brushColour);
         _drawComputeShader.SetTexture(updateKernel, "_Result", _canvasRenderTexture);
         _drawComputeShader.Dispatch(updateKernel, _canvasRenderTexture.width / 8,
             _canvasRenderTexture.height / 8, 1);
