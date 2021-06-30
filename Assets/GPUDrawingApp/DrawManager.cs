40be0913a8964829d6e8e5e041778290d85a3c0a
Bronson Zgeb <bronson.zgeb@gmail.com>
2021-06-30 16:14:05 -0400

    Added smoothing interval as a property

--- Assets/GPUDrawingApp/DrawManager.cs
+++ Assets/GPUDrawingApp/DrawManager.cs
@@ -6,16 +6,17 @@ public class DrawManager : MonoBehaviour
     [SerializeField] Color _backgroundColour;
     [SerializeField] Color _brushColour;
     [SerializeField] float _brushSize = 10f;
-    
+
     [SerializeField] BrushSizeSlider _brushSizeSlider;
+    [SerializeField, Range(0.01f, 1)] float _strokeSmoothingInterval = 0.1f;
     RenderTexture _canvasRenderTexture;
 
     Vector4 _previousMousePosition;
-    
+
     void Start()
     {
         _brushSizeSlider.slider.SetValueWithoutNotify(_brushSize);
-        
+
         _canvasRenderTexture = new RenderTexture(Screen.width, Screen.height, 24);
         _canvasRenderTexture.filterMode = FilterMode.Point;
         _canvasRenderTexture.enableRandomWrite = true;
@@ -32,18 +33,20 @@ public class DrawManager : MonoBehaviour
 
     void Update()
     {
-        if (_brushSizeSlider.isInUse) return;
+        if (!_brushSizeSlider.isInUse)
+        {
+            int updateKernel = _drawComputeShader.FindKernel("Update");
+            _drawComputeShader.SetVector("_PreviousMousePosition", _previousMousePosition);
+            _drawComputeShader.SetVector("_MousePosition", Input.mousePosition);
+            _drawComputeShader.SetBool("_MouseDown", Input.GetMouseButton(0));
+            _drawComputeShader.SetFloat("_BrushSize", _brushSize);
+            _drawComputeShader.SetVector("_BrushColour", _brushColour);
+            _drawComputeShader.SetFloat("_StrokeSmoothingInterval", _strokeSmoothingInterval);
+            _drawComputeShader.SetTexture(updateKernel, "_Result", _canvasRenderTexture);
+            _drawComputeShader.Dispatch(updateKernel, _canvasRenderTexture.width / 8,
+                _canvasRenderTexture.height / 8, 1);
+        }
         
-        int updateKernel = _drawComputeShader.FindKernel("Update");
-        _drawComputeShader.SetVector("_PreviousMousePosition", _previousMousePosition);
-        _drawComputeShader.SetVector("_MousePosition", Input.mousePosition);
-        _drawComputeShader.SetBool("_MouseDown", Input.GetMouseButton(0));
-        _drawComputeShader.SetFloat("_BrushSize", _brushSize);
-        _drawComputeShader.SetVector("_BrushColour", _brushColour);
-        _drawComputeShader.SetTexture(updateKernel, "_Result", _canvasRenderTexture);
-        _drawComputeShader.Dispatch(updateKernel, _canvasRenderTexture.width / 8,
-            _canvasRenderTexture.height / 8, 1);
-
         _previousMousePosition = Input.mousePosition;
     }
 
