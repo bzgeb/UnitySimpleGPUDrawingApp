16343642a1548085c74187837612d1d6a789f681
Bronson Zgeb <bronson.zgeb@gmail.com>
2021-06-30 15:15:37 -0400

    Brush size slider

---
+++ Assets/GPUDrawingApp/BrushSizeSlider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BrushSizeSlider : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    public bool isInUse;
+    public Slider slider;
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isInUse = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isInUse = true;
+    }
+}
--- Assets/GPUDrawingApp/DrawManager.cs
+++ Assets/GPUDrawingApp/DrawManager.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DrawManager : MonoBehaviour
 {
     [SerializeField] ComputeShader _drawComputeShader;
     [SerializeField] Color _backgroundColour;
     [SerializeField] Color _brushColour;
+    [SerializeField] float _brushSize = 10f;
+    
+    [SerializeField] BrushSizeSlider _brushSizeSlider;
     RenderTexture _canvasRenderTexture;
 
     void Start()
     {
+        _brushSizeSlider.slider.SetValueWithoutNotify(_brushSize);
+        
         _canvasRenderTexture = new RenderTexture(Screen.width, Screen.height, 24);
         _canvasRenderTexture.filterMode = FilterMode.Point;
         _canvasRenderTexture.enableRandomWrite = true;
@@ -19,20 +25,16 @@ public class DrawManager : MonoBehaviour
         _drawComputeShader.SetTexture(initBackgroundKernel, "_Result", _canvasRenderTexture);
         _drawComputeShader.Dispatch(initBackgroundKernel, _canvasRenderTexture.width / 8,
             _canvasRenderTexture.height / 8, 1);
-        /*
-        int mainKernel = _drawComputeShader.FindKernel("CSMain");
-        _drawComputeShader.SetTexture(mainKernel, "_Result", _canvasRenderTexture);
-        _drawComputeShader.Dispatch(mainKernel, _canvasRenderTexture.width / 8,
-            _canvasRenderTexture.height / 8, 1);
-            */
     }
 
     void Update()
     {
+        if (_brushSizeSlider.isInUse) return;
+        
         int updateKernel = _drawComputeShader.FindKernel("Update");
         _drawComputeShader.SetVector("_MousePosition", Input.mousePosition);
         _drawComputeShader.SetBool("_MouseDown", Input.GetMouseButton(0));
-        _drawComputeShader.SetFloat("_BrushSize", 10f);
+        _drawComputeShader.SetFloat("_BrushSize", _brushSize);
         _drawComputeShader.SetVector("_BrushColour", _brushColour);
         _drawComputeShader.SetTexture(updateKernel, "_Result", _canvasRenderTexture);
         _drawComputeShader.Dispatch(updateKernel, _canvasRenderTexture.width / 8,
@@ -42,5 +44,10 @@ public class DrawManager : MonoBehaviour
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
         Graphics.Blit(_canvasRenderTexture, dest);
+    }
+
+    public void OnBrushSizeChanged(float newValue)
+    {
+        _brushSize = newValue;
     }
 }
